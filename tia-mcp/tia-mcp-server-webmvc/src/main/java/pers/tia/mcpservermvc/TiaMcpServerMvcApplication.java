5cc448acb26d82f725275b4cede99b214e41bc93
zachary <zachary95526@gmail.com>
2025-09-09 17:37:38 +0800

    feat(mcp): mcp-server

---
+++ tia-mcp/tia-mcp-server-webmvc/src/main/java/pers/tia/mcpservermvc/TiaMcpServerMvcApplication.java
@@ -0,0 +1,13 @@
+package pers.tia.mcpservermvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiaMcpServerMvcApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TiaMcpServerMvcApplication.class, args);
+  }
+
+}
